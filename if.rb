c757bd4a99f96fd232d8925b3e7827cb83e81779
0902-max <moche.0830@icloud.com>
2024-01-21 23:14:58 +0900

    ruby超入門chapter3終了

--- if.rb
+++ if.rb
@@ -21,3 +21,33 @@ end
 if !x
     puts "if: xはfalseです"
 end
+wallet = 100
+if wallet >= 300
+    puts "コンビニでアイスを買っていこう！"
+else
+    puts "川沿いを散歩しよう！"
+end
+wallet = 500
+if wallet >= 300
+    puts "コンビニでアイスを買って行こう！"
+else
+    puts "川沿いを散歩しよう！"
+end
+season = "夏"
+if season == "春"
+    puts "アイスを買って行こう！"
+elsif season = "夏"
+    puts "かき氷を買って行こう！"
+else
+    puts "あんまん買って行こう！"
+end
+season = "夏"
+if season == "春"
+    puts "アイスを買って行こう！"
+else
+    if season == "夏"
+        puts "かき氷を買って行こう！"
+    else
+        puts "あんまんを買って行こう！"
+    end
+end
